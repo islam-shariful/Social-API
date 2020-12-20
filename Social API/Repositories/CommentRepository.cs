2af89886d79637a2656e390075a1c5f7668f34ff
Md. Shariful Islam <imdshariful171@gmail.com>
2020-12-20 14:18:10 +0600

    Entity DataModel+Repository

---
+++ Social API/Repositories/CommentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Social_API.Models;
+using Social_API.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory_with_Repository_Pattern.Repositories
+{
+    public class ProductRepository : Repository<Comment>
+    {
+    }
+}
---
+++ Social API/Repositories/PostRepository.cs
@@ -0,0 +1,12 @@
+﻿using Social_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Social_API.Repositories
+{
+    public class CategoryRepository : Repository<Post>
+    {
+    }
+}
